b094936b68deaed8a490bdc7a1a971a9c1fe6223
deddy11 <30648100+deddy11@users.noreply.github.com>
2017-11-13 20:51:42 +0700

    Change FSM make only 5 state

--- FSM/fsm.c
+++ FSM/fsm.c
@@ -18,77 +18,66 @@
 #define OFF_1XSTABLE	2
 #define ON_2XAWAL		3
 #define ON_2XSTABLE		4
-#define ON_INTERRUPT	5
 
 void fsm(int input, int *state, int *output, int *counter) {
-  switch(*state) {
-    case OFF_STABLE: {  //S0
-      if(input == 1) {
-        *state= OFF_1XAWAL;
-      }
-      break;
-    }
-    case OFF_1XAWAL: {  //S1
-      if(input == 0) {
-        *state = OFF_1XSTABLE;
-      }
-      break;
-    }
-    case OFF_1XSTABLE: {  //S2
-      if(input == 1) {
-        *state = ON_2XAWAL;
-        *counter = 0;
-      }
-      else {
-        *counter += 1;
-        if(*counter >= 1000) {
-          *state = OFF_STABLE;
-          *counter = 0;
-        }
-      }
-      break;
-    }
-    case ON_2XAWAL: { //S3
-      if(input == 0) {
-        *state = ON_2XSTABLE;
-      }
-      break;
-    }
-    case ON_2XSTABLE: { //S4
-      if(input == 1) {
-        *state = ON_INTERRUPT;
-        *counter = 0;
-      }
-    else {
-    *counter += 1;
-    if(*counter >= 5000) {
-      *state = OFF_STABLE;
-      *counter = 0;
-    }  
-    }
-      break;
-    }
-    case ON_INTERRUPT: {  //S5
-      if(input == 0) {
-        *state = ON_2XSTABLE;
-      }
-      break;
-    }
-  }
+	switch(*state) {
+		case OFF_STABLE: {  //S0
+			*counter = 0;
+			if(input == 1) {
+				*state= OFF_1XAWAL;
+			}
+			break;
+		}
+		case OFF_1XAWAL: {  //S1
+			if(input == 0) {
+				*state = OFF_1XSTABLE;
+			}
+			break;
+		}
+		case OFF_1XSTABLE: {  //S2
+			if(input == 1) {
+				*state = ON_2XAWAL;
+			}
+			else {
+			*counter += 1;
+				if(*counter >= 1000) {
+					*state = OFF_STABLE;
+				}
+			}
+			break;
+		}
+		case ON_2XAWAL: { //S3
+			*counter = 0;
+			if(input == 0) {
+				*state = ON_2XSTABLE;
+			}
+			break;
+		}
+		case ON_2XSTABLE: { //S4
+			if(input == 1) {
+				*state = ON_2XAWAL;
+			}
+			else {
+			*counter += 1;
+				if(*counter >= 5000) {
+					*state = OFF_STABLE;
+				}  
+			}
+			break;
+		}
+	}
   
-  switch(*state) {
-    case OFF_STABLE:	//S0
-    case OFF_1XAWAL:	//S1
-    case OFF_1XSTABLE:{	//S2
-		*output = 0;
-		break;
+	switch(*state) {
+		case OFF_STABLE:	//S0
+		case OFF_1XAWAL:	//S1
+		case OFF_1XSTABLE:{	//S2
+			*output = 0;
+			break;
+		}
+		case ON_2XAWAL:		//S3
+		case ON_2XSTABLE: {	//S4
+			*output = 1;
+			break;
+		}
 	}
-    case ON_2XAWAL:		//S3
-	case ON_2XSTABLE:	//S4
-    case ON_INTERRUPT:{	//S5
-		*output = 1;
-		break;
-	}
-  }
-
 }
--- Tester/fsm.c
+++ Tester/fsm.c
@@ -3,7 +3,7 @@
 #include "fsm.h"
 
 // file ini berisi FSM yang akan diuji
-// frekuensi FSM 10 Hz
+// frekuensi FSM 1000 Hz
 
 /**
  * FSM under test
@@ -18,83 +18,66 @@
 #define OFF_1XSTABLE	2
 #define ON_2XAWAL		3
 #define ON_2XSTABLE		4
-#define ON_INTERRUPT	5
 
 void fsm(int input, int *state, int *output, int *counter) {
-  switch(*state) {
-    case OFF_STABLE: {  //S0
-      printf("State: OFF_STABLE\n");
-      if(input == 1) {
-        *state= OFF_1XAWAL;
-      }
-      break;
-    }
-    case OFF_1XAWAL: {  //S1
-      printf("State: OFF_1XAWAL\n");
-      if(input == 0) {
-        *state = OFF_1XSTABLE;
-      }
-      break;
-    }
-    case OFF_1XSTABLE: {  //S2
-      printf("State: OFF_1XSTABLE\n");
-      if(input == 1) {
-        *state = ON_2XAWAL;
-        *counter = 0;
-      }
-      else {
-        *counter += 1;
-        if(*counter >= 10) {
-          *state = OFF_STABLE;
-          *counter = 0;
-        }
-      }
-      break;
-    }
-    case ON_2XAWAL: { //S3
-      printf("State: ON_2XAWAL\n");
-      if(input == 0) {
-        *state = ON_2XSTABLE;
-      }
-      break;
-    }
-    case ON_2XSTABLE: { //S4
-      printf("State: ON_2XSTABLE\n");
-      if(input == 1) {
-        *state = ON_INTERRUPT;
-        *counter = 0;
-      }
-    else {
-    *counter += 1;
-    if(*counter >= 50) {
-      *state = OFF_STABLE;
-      *counter = 0;
-    }  
-    }
-      break;
-    }
-    case ON_INTERRUPT: {  //S5
-      printf("State: ON_INTERRUPT\n");
-      if(input == 0) {
-        *state = ON_2XSTABLE;
-      }
-      break;
-    }
-  }
+	switch(*state) {
+		case OFF_STABLE: {  //S0
+			*counter = 0;
+			if(input == 1) {
+				*state= OFF_1XAWAL;
+			}
+			break;
+		}
+		case OFF_1XAWAL: {  //S1
+			if(input == 0) {
+				*state = OFF_1XSTABLE;
+			}
+			break;
+		}
+		case OFF_1XSTABLE: {  //S2
+			if(input == 1) {
+				*state = ON_2XAWAL;
+			}
+			else {
+			*counter += 1;
+				if(*counter >= 1000) {
+					*state = OFF_STABLE;
+				}
+			}
+			break;
+		}
+		case ON_2XAWAL: { //S3
+			*counter = 0;
+			if(input == 0) {
+				*state = ON_2XSTABLE;
+			}
+			break;
+		}
+		case ON_2XSTABLE: { //S4
+			if(input == 1) {
+				*state = ON_2XAWAL;
+			}
+			else {
+			*counter += 1;
+				if(*counter >= 5000) {
+					*state = OFF_STABLE;
+				}  
+			}
+			break;
+		}
+	}
   
-  switch(*state) {
-    case OFF_STABLE:	//S0
-    case OFF_1XAWAL:	//S1
-    case OFF_1XSTABLE:{	//S2
-		*output = 0;
-		break;
+	switch(*state) {
+		case OFF_STABLE:	//S0
+		case OFF_1XAWAL:	//S1
+		case OFF_1XSTABLE:{	//S2
+			*output = 0;
+			break;
+		}
+		case ON_2XAWAL:		//S3
+		case ON_2XSTABLE: {	//S4
+			*output = 1;
+			break;
+		}
 	}
-    case ON_2XAWAL:		//S3
-	case ON_2XSTABLE:	//S4
-    case ON_INTERRUPT:{	//S5
-		*output = 1;
-		break;
-	}
-  }
-
 }
